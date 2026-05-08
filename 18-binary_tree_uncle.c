2de341565335ec8072f8de32c8f84ba1e31ec5f5
AfnanAljohani <13258@holbertonstudents.com>
2026-05-08 23:53:53 +0000

    Fix task 18 uncle - make it independent

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,9 +1,18 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_uncle - Finds the uncle of a node
+ * @node: Pointer to the node to find the uncle
+ *
+ * Return: Pointer to the uncle node, or NULL if node is NULL,
+ * if parent is NULL, or if node has no uncle
+ */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	if (node == NULL || node->parent == NULL)
+	if (node == NULL || node->parent == NULL ||
+	    node->parent->parent == NULL)
 		return (NULL);
-
-	return (binary_tree_sibling(node->parent));
+	if (node->parent->parent->left == node->parent)
+		return (node->parent->parent->right);
+	return (node->parent->parent->left);
 }
